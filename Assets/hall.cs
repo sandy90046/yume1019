479f93bcad46f0801ce32dc98b9a775c8bac160c
peggy87222 <peggy87222@gmail.com>
2018-09-26 03:02:56 +0800

    add running

---
+++ Assets/hall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hall : MonoBehaviour
+{
+       
+    private CharacterController Cc;
+    private float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+    void Awake() {
+        Cc = gameObject.GetComponent<CharacterController>();
+        speed = 3.0f;
+    }
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            Cc.Move(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
