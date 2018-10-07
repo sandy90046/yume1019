1712f4b9ac0dab742fa9e639789931b9fd99bae7
peggy87222 <peggy87222@gmail.com>
2018-10-07 14:36:06 +0800

    add move

---
+++ Assets/script/VRTouchpadMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-------------------------------------------------------------------------------------------------\\
+// This code has been developed by Feisty Crab Studios for personal, commercial, and education use.\\
+//                                                                                                 \\
+// You are free to edit and redistribute this code, subject to the following:                      \\
+//                                                                                                 \\
+//      1. You will not sell this code or an edited version of it.                                 \\
+//      2. You will not remove the copyright messages                                              \\
+//      3. You will give credit to Feisty Crab Studios if used commercially                        \\
+//      4. Don't be a mean sausage, nobody likes a mean sausage.                                   \\
+//                                                                                                 \\
+// Contact us @ feistycrabstudios.gmail.com with any questions.                                    \\
+//-------------------------------------------------------------------------------------------------\\
+
+public class VRTouchpadMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform rig;
+    public float speed = 100f;
+    private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    private Vector2 axis = Vector2.zero;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (controller.GetTouch(touchpad))
+        {
+            axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            if (rig != null)
+            {
+                rig.position += (transform.right * axis.x + transform.forward * axis.y) * Time.deltaTime* speed;
+                rig.position = new Vector3(rig.position.x, 30, rig.position.z);
+            }
+        }
+
+    }
+}
