7cccffd902e0f5e47328e449a8d28d23ced2eb7d
peggy87222 <peggy87222@gmail.com>
2018-10-13 19:48:20 +0800

    add sence boy

---
+++ Assets/factory/sharder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sharder : MonoBehaviour
+{
+	void Update()
+	{
+		RaycastHit[] hits;
+		hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+		int i = 0;
+		while (i < hits.Length)
+		{
+			RaycastHit hit = hits[i];
+			Renderer renderer = hit.collider.GetComponent<Renderer>();
+			if (renderer)
+			{
+				renderer.material.shader = Shader.Find("Transparent/Diffuse");
+				renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.4f);
+
+			}
+			i++;
+		}
+	}
+}
